91d26936c5f79e71ab107a1985d7d882fa2a863b
Rob Wierzbowski <hello@robwierzbowski.com>
2015-04-17 16:55:39 -0400

    moar waps

--- fixture/gulpfile.js
+++ fixture/gulpfile.js
@@ -4,27 +4,53 @@ var gulp = require('gulp');
 var sourcemaps = require('gulp-sourcemaps');
 var sass = require('../');
 
-// inline sourcemaps
-gulp.task('sass-inline', function() {
-	return sass('source', { sourcemap: true, verbose: true })
+gulp.task('sass', function() {
+	return sass('sourcez', { verbose: true })
 	.on('error', function (err) {
-	  console.error('Error!', err.message);
-   })
-	.pipe(sourcemaps.write())
+	 console.error('Error!', err.message);
+  })
+	// .pipe(sourcemaps.write())
+	.pipe(gulp.dest('result'));
+});
+
+gulp.task('sass-be', function() {
+	return sass('sourcez', { bundleExec: true, verbose: true })
+	.on('error', function (err) {
+	 console.error('Error!', err.message);
+  })
+	.pipe(gulp.dest('result'));
+});
+
+// Files VV
+
+gulp.task('sassf', function() {
+	return sass('source/fixture-a.css', { verbose: true })
+	.on('error', function (err) {
+	 console.error('Error!', err.message);
+  })
+	// .pipe(sourcemaps.write())
+	.pipe(gulp.dest('result'));
+});
+
+gulp.task('sassf-be', function() {
+	return sass('source/fixture-a.css', { bundleExec: true, verbose: true })
+	.on('error', function (err) {
+	 console.error('Error!', err.message);
+  })
 	.pipe(gulp.dest('result'));
 });
 
 // file sourcemaps
-gulp.task('sass-file', function() {
-	return sass('source', { sourcemap: true })
-	.on('error', function (err) {
-	  console.error('Error!', err.message);
-   })
-
-	.pipe(sourcemaps.write('../maps', {
-		includeContent: false,
-		sourceRoot: '/fixture/source'
-	}))
-
-	.pipe(gulp.dest('result'));
-});
+// gulp.task('sass-file', function() {
+//	return sass('source', { sourcemap: true })
+// 	.on('error', function (err) {
+// 	  console.error('Error!', err.message);
+//    })
+//
+// 	.pipe(sourcemaps.write('../maps', {
+// 		includeContent: false,
+// 		sourceRoot: '/fixture/source'
+// 	}))
+//
+// 	.pipe(gulp.dest('result'));
+// });
